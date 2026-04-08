16a1c3a3eff63502b33fdb5ed9dc055b3893e216
joaovctr010 <joaovctr93@gmail.com>
2026-04-08 20:01:57 -0300

    Adicionando linhas a um codigo

--- Lógica de Programação/Material/Codigos JS/funcoes_com_parametro.js
+++ Lógica de Programação/Material/Codigos JS/funcoes_com_parametro.js
@@ -1,7 +1,13 @@
-function torrar(pao){
-    console.log("torrada feita com "+pao)
+
+
+
+function torrar(pao = "Comum", nome = "Cliente", valor = 0.0){
+    console.log("torrada feita com pão "+pao)
+    console.log("Ele é um pedido de: "+nome)
+    console.log("O Valor total é: "+valor)
 }
 
-torrar("pão integral")
-torrar("pão de forma")
-torrar("pão brioche")
+torrar()
+torrar("integral", "João", 15.9)
+torrar("de forma", "Ysadora", 25)
+torrar("brioche", "Maria Eduarda", 50)
